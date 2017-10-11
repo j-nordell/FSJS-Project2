4b0c08e23f8b9e8e826e28c633e3461d349cdce3
Jennifer Nordell <jennifer.nordell@outlook.com>
2017-10-11 12:30:17 +0200

    pagination mostly functional

--- js/script.js
+++ js/script.js
@@ -3,30 +3,69 @@
 // Project theme: Pagination
 
 let studentItems = document.querySelectorAll(".student-item");
+let offset = 10;
 
-showPage(1, studentItems);
+
+
+appendPageLinks(studentItems);
+//showPage(1, studentItems);
 
 function showPage(pageNumber, studentList) {
+    let pageLinks = document.querySelectorAll(".pagination ul li a");
+    for(var i = 0; i < pageLinks.length; i++) {
+        pageLinks[i].className.replace("active", "");
+    }
+    pageLinks[pageNumber - 1].className = "active";
+
     for(var i = 0; i < studentList.length; i++) {
         studentList[i].style.display = "none";
     }
     // first hide all students on the page
     // Then loop through all students in our student list argument
        // if student should be on this page number
-       	// show the student
+           // show the student
+    if(pageNumber == Math.ceil(studentList.length / offset)) {
+        for(var i = (pageNumber * offset) - offset; i < studentList.length; i++) {
+            studentList[i].style.display = "block";
+        }
+    } else {
+        for(var i = (pageNumber*offset) - offset; i < (pageNumber * offset); i++) {
+        studentList[i].style.display = "block";
+        }   
+    }         
+
  }
 
  function appendPageLinks(studentList) {
     // determine how many pages for this student list
-    let numOfPages = 1;
-    if(studentList.length % 10 == 0) {
-        numOfPages = studentList.length / 10;
-    } else {
-        numOfPages = Math.floor(studentList.length / 10) + 1;
-    }
+    let numOfPages;
+    let pageNode = document.querySelector(".page");
+    let pageDiv = document.createElement("div");
+    let pageList = document.createElement("ul");
+    let pageNumber = document.createElement("li");
+    
+
+    numOfPages = Math.ceil(studentList.length / 10);
+
     // create a page link section
+    pageNode.appendChild(pageDiv);
+    pageDiv.appendChild(pageList);
+    pageDiv.className = "pagination";
     // “for” every page
+    for(let i = 1; i <= numOfPages; i++) {
         // add a page link to the page link section
+        let pageLink = document.createElement("a");
+        let pageNumberText = document.createTextNode(i);
+        pageLink.setAttribute("href", "#");
+        pageLink.addEventListener("click", function() {
+             showPage(i, studentList);
+        });
+        pageList.appendChild(pageNumber);
+        pageNumber.appendChild(pageLink);
+        pageLink.appendChild(pageNumberText);  
+    }
+
+    
     // remove the old page link section from the site
     // append our new page link section to the site
     // define what happens when you click a link
