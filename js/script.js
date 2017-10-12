28b03807b72212042e9fa9e20ae8117c8e091ae0
Jennifer Nordell <jennifer.nordell@outlook.com>
2017-10-12 09:36:21 +0200

    addition of pseudocode

--- js/script.js
+++ js/script.js
@@ -75,3 +75,19 @@ function showPage(pageNumber, studentList) {
         pageLink.appendChild(pageNumberText);  //append the text to the link
     }
 }
+
+function searchList() {
+    // Obtain the value of the search input
+    // remove the previous page link section    
+    // Loop over the student list, and for each student…
+// ...obtain the student’s name…
+// ...and the student’s email…
+// ...if the search value is found inside either email or name…
+    		// ...add this student to list of “matched” student
+    // If there’s no “matched” students…
+           // ...display a “no student’s found” message
+    // If over ten students were found…
+           // ...call appendPageLinks with the matched students
+   // Call showPage to show first ten students of matched list
+}
+
